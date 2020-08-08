6b8549ba44408818f9d5094935b24c9216be7c25
AanyaBhatia <69009364+AanyaBhatia@users.noreply.github.com>
2020-08-08 09:56:46 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,30 +1,30 @@
-var fixedRect, movingRect;
+
 
 function setup() {
   createCanvas(1200,800);
-  fixedRect = createSprite(400, 100, 50, 80);
-  fixedRect.shapeColor = "green";
-  fixedRect.debug = true;
-  movingRect = createSprite(400, 800,80,30);
-  movingRect.shapeColor = "green";
-  movingRect.debug = true;
-
-  movingRect.velocityY = -5;
-  fixedRect.velocityY = +5;
+  fixedRect=createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor="green";
+  fixedRect.debug=true;
+  movingRect=createSprite(400,800,80,30);
+  movingRect.shapeColor="green";
+  movingRect.debug=true;
+  movingRect.velocityY=-5;
+  fixedRect.velocityY=5;
+  object1=createSprite(340,125,50,50);
+  object2=createSprite(340,125,50,50);
 }
 
 function draw() {
-  background(0,0,0);  
-
-  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
-      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2) {
-    movingRect.velocityX = movingRect.velocityX * (-1);
-    fixedRect.velocityX = fixedRect.velocityX * (-1);
-  }
-  if (movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
-    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
-    movingRect.velocityY = movingRect.velocityY * (-1);
-    fixedRect.velocityY = fixedRect.velocityY * (-1);
+  background(0,0,0); 
+  object1.y=World.mouseY;
+  object1.x=World.mouseX; 
+  bounceOff(movingRect,fixedRect);
+  if(isTouching(object1,object2)){
+    object1.shapeColor="red";
+    object2.shapeColor="red";
+  } else {
+    object1.shapeColor="green";
+    object2.shapeColor="green";
   }
   drawSprites();
 }
